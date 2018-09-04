b5b169e9be89d2d8b92ab915d8c9799cb8d016c8
EducationEKT <xlgfzh@gmail.com>
2018-09-04 15:14:59 +0800

    bug fixed: from == to

--- dispatcher/dispatcher.go
+++ dispatcher/dispatcher.go
@@ -1,6 +1,7 @@
 package dispatcher
 
 import (
+	"bytes"
 	"encoding/hex"
 	"errors"
 
@@ -25,6 +26,9 @@ func NewTransaction(transaction *userevent.Transaction) error {
 	}
 	if !userevent.Validate(transaction) {
 		return errors.New("error signature")
+	}
+	if bytes.EqualFold(transaction.GetFrom(), transaction.GetTo()) {
+		return errors.New("invalid address")
 	}
 	blockchain_manager.GetMainChain().NewTransaction(transaction)
 	return nil
