d61d00428f0533f623db85b9e611870fedb181e4
EducationEKT <xlgfzh@gmail.com>
2018-09-18 18:44:07 +0800

    后期增加多种RPC的通用模块

--- service/service.go
+++ service/service.go
@@ -1,15 +1,35 @@
 package service
 
+const (
+	PARAM_TYPE_BODY = iota
+	PARAM_TYPE_INT
+	PARAM_TYPE_FLOAT64
+	PARAM_TYPE_STRING
+)
+
+const (
+	PARAM_FROM_QUERY = iota
+	PARAM_FROM_BODY
+	PARAM_FROM_ALL
+)
+
 type Service interface {
 	Start() error
 	Stop() error
 }
 
-type RPCFunc func(param interface{}) interface{}
+type RPCFunc func(params ...interface{}) interface{}
 
 type Func struct {
 	FuncName string
+	Params   []Param
 	Func     RPCFunc
+}
+
+type Param struct {
+	Name string
+	From int
+	Type int
 }
 
 type FuncGroup struct {
