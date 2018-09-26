9ff9c888a716b006037965954aab5933b0e7c6ca
EducationEKT <xlgfzh@gmail.com>
2018-09-26 20:23:31 +0800

    round中增加节点心跳时间，增加出块稳定性

--- core/types/round.go
+++ core/types/round.go
@@ -1,8 +1,31 @@
 package types
 
+import "sync"
+
 type Round struct {
-	CurrentIndex int    `json:"currentIndex"` // default -1
-	Peers        []Peer `json:"peers"`
+	CurrentIndex int          `json:"currentIndex"` // default -1
+	Peers        []Peer       `json:"peers"`
+	time         int64        `json:"-"`
+	Locker       sync.RWMutex `json:"-"`
+}
+
+func NewRound(peers Peers, index int, time int64) *Round {
+	return &Round{
+		CurrentIndex: index,
+		Peers:        peers,
+		time:         time,
+		Locker:       sync.RWMutex{},
+	}
+}
+
+func (round *Round) SetTime(time int64) {
+	round.Locker.Lock()
+	round.time = time
+	round.Locker.Unlock()
+}
+
+func (round Round) GetTime() int64 {
+	return round.time
 }
 
 func (round *Round) UpdateIndex(miner string) {
@@ -20,4 +43,14 @@ func (round Round) IndexOf(miner string) int {
 
 func (round Round) Len() int {
 	return len(round.Peers)
+}
+
+func (round Round) Clone() Round {
+	round.Locker.RLock()
+	defer round.Locker.RUnlock()
+	return Round{
+		CurrentIndex: round.CurrentIndex,
+		Peers:        round.Peers,
+		time:         round.time,
+	}
 }
