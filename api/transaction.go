4d9495c57ef9eeea26687cc671ca3776e0734e0e
EducationEKT <xlgfzh@gmail.com>
2018-09-27 17:11:20 +0800

    删除获取交易状态的接口

--- api/transaction.go
+++ api/transaction.go
@@ -1,14 +1,12 @@
 package api
 
 import (
-	"encoding/hex"
 	"encoding/json"
 	"github.com/EducationEKT/EKT/core/userevent"
 	"github.com/EducationEKT/EKT/crypto"
 	"github.com/EducationEKT/EKT/db"
 	"github.com/EducationEKT/EKT/dispatcher"
 	"github.com/EducationEKT/EKT/node"
-	"github.com/EducationEKT/EKT/param"
 	"github.com/EducationEKT/xserver/x_err"
 	"github.com/EducationEKT/xserver/x_http/x_req"
 	"github.com/EducationEKT/xserver/x_http/x_resp"
@@ -19,7 +17,6 @@ func init() {
 	x_router.Get("/transaction/api/fee", fee)
 	x_router.Post("/transaction/api/newTransaction", broadcast, newTransaction)
 	x_router.Get("/transaction/api/userTxs", userTxs)
-	x_router.Get("/transaction/api/status", txStatus)
 }
 
 func fee(req *x_req.XReq) (*x_resp.XRespContainer, *x_err.XErr) {
@@ -29,36 +26,6 @@ func fee(req *x_req.XReq) (*x_resp.XRespContainer, *x_err.XErr) {
 func userTxs(req *x_req.XReq) (*x_resp.XRespContainer, *x_err.XErr) {
 	address := req.MustGetString("address")
 	return x_resp.Return(node.GetMainChain().Pool.GetUserTxs(address), nil)
-}
-
-func txStatus(req *x_req.XReq) (*x_resp.XRespContainer, *x_err.XErr) {
-	// get transaction by transactionId
-	transactionId := req.MustGetString("txId")
-	txId, err := hex.DecodeString(transactionId)
-	if err != nil {
-		return x_resp.Return(nil, err)
-	}
-	if tx := userevent.GetTransaction(txId); tx == nil {
-		synchronizeTransaction(txId)
-	}
-	tx := userevent.GetTransaction(txId)
-	if tx == nil {
-		return x_resp.Return("error transaction not found", nil)
-	}
-
-	// get account by address
-	account, err := node.GetMainChain().LastHeader().GetAccount(tx.GetFrom())
-	if err != nil {
-		return x_resp.Return(nil, err)
-	}
-
-	// transaction has been processed
-	if account.Nonce >= tx.Nonce {
-		// 200 = processed
-		return x_resp.Return(200, nil)
-	}
-	// 100 = pending
-	return x_resp.Return(100, nil)
 }
 
 func newTransaction(req *x_req.XReq) (*x_resp.XRespContainer, *x_err.XErr) {
@@ -76,12 +43,4 @@ func newTransaction(req *x_req.XReq) (*x_resp.XRespContainer, *x_err.XErr) {
 		db.GetDBInst().Set(txId, tx.Bytes())
 	}
 	return x_resp.Return(tx.TransactionId(), err)
-}
-
-func synchronizeTransaction(txId []byte) {
-	for _, peer := range param.MainChainDelegateNode {
-		if value, err := peer.GetDBValue(hex.EncodeToString(txId)); err != nil {
-			db.GetDBInst().Set(txId, value)
-		}
-	}
 }
