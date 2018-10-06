71920df39d5ffb88faf9c05df5a155caed2b1481
EducationEKT <xlgfzh@gmail.com>
2018-10-06 23:01:58 +0800

    修改初始化合约的代码

--- contract/types.go
+++ contract/types.go
@@ -5,6 +5,7 @@ import (
 	"github.com/EducationEKT/EKT/bancor"
 	"github.com/EducationEKT/EKT/core/types"
 	"github.com/EducationEKT/EKT/core/userevent"
+	"math"
 	"sync"
 )
 
@@ -28,41 +29,39 @@ type Contracts struct {
 	locker sync.RWMutex
 }
 
-func GetContract(address []byte, account *types.Account) Contract {
+func getContract(address []byte, account *types.Account) Contract {
 	contracts.locker.RLock()
-	contract, inited := contracts.m[hex.EncodeToString(address)]
+	contract, exist := contracts.m[hex.EncodeToString(address)]
 	contracts.locker.RUnlock()
 
-	//subAddress := address[32:]
-	if !inited {
-		return initContract(address)
-		//if account.Contracts != nil {
-		//	c, exist := account.Contracts[hex.EncodeToString(subAddress)]
-		//	if !exist {
-		//		contract = initContract(address)
-		//	} else {
-		//		contract.Recover(c.ContractData)
-		//	}
-		//} else {
-		//	contract = initContract(address)
-		//}
+	if !exist {
+		contract = initContract(address, account)
 	}
 
 	return contract
 }
 
-func initContract(address []byte) Contract {
+func updateContract(address []byte, c Contract) {
+	contracts.locker.Lock()
+	contracts.m[hex.EncodeToString(address)] = c
+	contracts.locker.Unlock()
+}
+
+func initContract(address []byte, account *types.Account) Contract {
 	author, subAddress := address[:32], address[32:]
 	switch hex.EncodeToString(author) {
 	case SYSTEM_AUTHOR:
 		switch hex.EncodeToString(subAddress) {
 		case EKT_GAS_BANCOR_CONTRACT:
-			return bancor.NewBancor(EKT_GAS_PARAM_CW, EKT_GAS_PARAM_INIT_CONNECT_TOKEN, EKT_GAS_PARAM_INIT_SMART_TOKEN, EKT_GAS_PARAM_TOTAL_SMART_TOKEN, types.EKTAddress, types.GasAddress)
+			b := bancor.NewBancor(EKT_GAS_PARAM_CW, math.Pow10(types.EKT_DECIMAL), EKT_GAS_PARAM_INIT_SMART_TOKEN, EKT_GAS_PARAM_TOTAL_SMART_TOKEN, types.EKTAddress, types.GasAddress)
+			contractAccount := account.Contracts[hex.EncodeToString(subAddress)]
+			if contractAccount.ContractData != nil {
+				b.Recover(contractAccount.ContractData)
+			}
+			return b
 		default:
-			// TODO other contract not open now
 		}
 	default:
-		// TODO user contract not open now
 	}
 	return nil
 }
