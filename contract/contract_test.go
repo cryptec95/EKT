dfd27d16276191cd7f46d120238cde931db37378
EducationEKT <xlgfzh@gmail.com>
2018-10-08 22:08:46 +0800

    update test

--- contract/contract_test.go
+++ contract/contract_test.go
@@ -1,6 +1,10 @@
 package contract
 
 import (
+	"fmt"
+	"github.com/EducationEKT/EKT/bancor"
+	"github.com/EducationEKT/EKT/core/types"
+	"math"
 	"testing"
 )
 
@@ -8,4 +12,10 @@ func TestCall(t *testing.T) {
 	//to, _ := hex.DecodeString(strings.Join([]string{strings.Repeat("0", 127), "1"}, ""))
 	//tx := userevent.NewTransaction([]byte(""), to, 0, 0, 0, 0, "", "")
 	//fmt.Println(Run(*tx))
+}
+
+func TestInitContractAccount(t *testing.T) {
+	bancor := bancor.NewBancor(EKT_GAS_PARAM_CW, math.Pow10(types.EKT_DECIMAL), EKT_GAS_PARAM_INIT_SMART_TOKEN, EKT_GAS_PARAM_TOTAL_SMART_TOKEN, types.EKTAddress, types.GasAddress)
+	fmt.Println(bancor.Buy(1000000000000))
+	fmt.Println(bancor.Buy(1000000000000))
 }
