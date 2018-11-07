1b463213b4a7bb88755573c4b6be826db3a00dc9
EducationEKT <xlgfzh@gmail.com>
2018-11-07 20:47:46 +0800

    add Sticker

---
+++ context/sticker_test.go
@@ -0,0 +1,27 @@
+package context
+
+import (
+	"encoding/hex"
+	"fmt"
+	"github.com/EducationEKT/EKT/crypto"
+	"sync"
+	"testing"
+)
+
+func TestSticker_Get(t *testing.T) {
+	sticker := Sticker{
+		m: &sync.Map{},
+	}
+	sticker.Save("hello", crypto.Sha3_256([]byte("123456")))
+	value := sticker.Get("hello")
+	if value == nil {
+		t.FailNow()
+	} else {
+		bytes, ok := value.([]byte)
+		if !ok {
+			t.FailNow()
+		} else {
+			fmt.Println(hex.EncodeToString(bytes))
+		}
+	}
+}
