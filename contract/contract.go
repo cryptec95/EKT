56163d3a1e26ea2780ff3d945679bac862b00910
EducationEKT <xlgfzh@gmail.com>
2018-11-07 20:50:08 +0800

    init contract

--- contract/contract.go
+++ contract/contract.go
@@ -2,8 +2,14 @@ package contract
 
 import (
 	"encoding/hex"
+	"encoding/json"
+
+	"github.com/EducationEKT/EKT/context"
 	"github.com/EducationEKT/EKT/core/types"
 	"github.com/EducationEKT/EKT/core/userevent"
+	"github.com/EducationEKT/EKT/crypto"
+	"github.com/EducationEKT/EKT/db"
+	"github.com/EducationEKT/EKT/vm"
 )
 
 func Run(tx userevent.Transaction, account *types.Account) (*userevent.TransactionReceipt, []byte) {
@@ -39,4 +45,35 @@ func InitContractAccount(tx userevent.Transaction, account *types.Account) bool 
 		}
 	}
 	return false
+}
+
+func InitContract(sticker *context.Sticker, account *types.Account, tx userevent.Transaction) (types.HexBytes, types.HexBytes, error) {
+	previousHash, _ := sticker.GetBytes("previousHash")
+	timestamp, _ := sticker.GetInt64("timestamp")
+
+	contractHash := crypto.Sha3_256([]byte(tx.Data))
+	db.GetDBInst().Set(contractHash, []byte(tx.Data))
+
+	evm := vm.NewVM(previousHash, timestamp)
+	_, err := evm.Run(tx.Data)
+	if err != nil {
+		return nil, contractHash, err
+	}
+
+	value, err := evm.Run(`
+		init();
+		return JSON.stringify({
+			"prop": prop;
+			"contract": contract
+		});
+	`)
+	if err != nil {
+		return nil, contractHash, err
+	}
+	var result ContractData
+	err = json.Unmarshal([]byte(value.String()), &result)
+	if err != nil {
+		return nil, contractHash, err
+	}
+	return result.Bytes(), contractHash, nil
 }
