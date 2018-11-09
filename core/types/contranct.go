ac4405d45ca957faac7e7dc27e28c1e02fa9d640
EducationEKT <xlgfzh@gmail.com>
2018-11-09 20:47:13 +0800

    refact contractAccount.ContractData

--- core/types/contranct.go
+++ core/types/contranct.go
@@ -1,8 +1,22 @@
 package types
 
+import (
+	"encoding/json"
+)
+
 // Contract address contains 64 bytes
 // The first 32 byte represents the founders of contract, it is create by system if the first
 // The end 32 byte represents the true address of contract for a founder
+
+type ContractData struct {
+	Prop     string `json:"prop"`
+	Contract string `json:"contract"`
+}
+
+func (data ContractData) Bytes() []byte {
+	result, _ := json.Marshal(data)
+	return result
+}
 
 type ContractAccount struct {
 	Address      HexBytes         `json:"address"`
@@ -13,13 +27,13 @@ type ContractAccount struct {
 	Balances     map[string]int64 `json:"balances"`
 }
 
-func NewContractAccount(address []byte, contractHash, contractData []byte) *ContractAccount {
+func NewContractAccount(address []byte, contractHash []byte, contractData ContractData) *ContractAccount {
 	return &ContractAccount{
 		Address:      address,
 		Amount:       0,
 		Gas:          0,
 		Balances:     make(map[string]int64),
 		CodeHash:     contractHash,
-		ContractData: contractData,
+		ContractData: contractData.Bytes(),
 	}
 }
