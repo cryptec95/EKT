fe394f4138f62eb4bc7bdb8582beba93ea494cdb
EducationEKT <xlgfzh@gmail.com>
2018-11-11 16:14:52 +0800

    update testnet

--- param/testnet.go
+++ param/testnet.go
@@ -4,5 +4,5 @@ import "github.com/EducationEKT/EKT/core/types"
 
 // TestNet，测试网内测和公测的Delegate节点
 var TestNet = []types.Peer{
-	{"968b10ebc111ea3434de7333d82e54890c4a2d8c34577e0e54f3464eb88e3b2f", "58.83.148.229", 19951, 4},
+	{"968b10ebc111ea3434de7333d82e54890c4a2d8c34577e0e54f3464eb88e3b2f", "124.251.0.232", 19941, 4},
 }
