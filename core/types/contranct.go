cfa3b9f22c99655230e34838853e2c3b8c993664
zhouxun <806275025@qq.com>
2018-11-12 21:48:58 +0800

    add AccountChange

--- core/types/contranct.go
+++ core/types/contranct.go
@@ -37,3 +37,34 @@ func NewContractAccount(address []byte, contractHash []byte, contractData Contra
 		ContractData: contractData.Bytes(),
 	}
 }
+
+func (account ContractAccount) Transfer(change AccountChange) bool {
+	for tokenAddr, amount := range change.M {
+		switch tokenAddr {
+		case EKTAddress:
+			account.Amount += amount
+			if account.Amount < 0 {
+				return false
+			}
+		case GasAddress:
+			account.Gas += amount
+			if account.Gas < 0 {
+				return false
+			}
+		default:
+			if account.Balances == nil {
+				account.Balances = make(map[string]int64)
+			}
+			count, exist := account.Balances[tokenAddr]
+			if !exist {
+				count = 0
+			}
+			count += amount
+			if count < 0 {
+				return false
+			}
+			account.Balances[tokenAddr] = count
+		}
+	}
+	return true
+}
