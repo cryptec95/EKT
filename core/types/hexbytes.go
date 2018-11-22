07f96287b8a2f9c1ba8bc70a4e2503ab2fae64ca
zhouxun <xlgfzh@gmail.com>
2018-11-22 15:24:06 +0800

    panic -> error

--- core/types/hexbytes.go
+++ core/types/hexbytes.go
@@ -2,12 +2,17 @@ package types
 
 import (
 	"encoding/hex"
+	"errors"
 	"fmt"
 )
 
 type HexBytes []byte
 
 func (hexBytes *HexBytes) UnmarshalJSON(data []byte) error {
+	if len(data) <= 2 {
+		*hexBytes = make([]byte, 0)
+		return errors.New("invalid HexBytes")
+	}
 	data = data[:len(data)-1][1:]
 	bytes, err := hex.DecodeString(string(data))
 	*hexBytes = bytes
