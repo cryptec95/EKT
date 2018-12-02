b4c4b59656ab3e68ff2ff3a1a8492d118df3c24b
zhouxun <xlgfzh@gmail.com>
2018-12-02 12:06:54 +0800

    package refact

--- encapdb/transaction.go
+++ encapdb/transaction.go
@@ -4,16 +4,16 @@ import (
 	"encoding/json"
 	"github.com/EducationEKT/EKT/core/userevent"
 	"github.com/EducationEKT/EKT/db"
-	"github.com/EducationEKT/EKT/encapdb/schema"
+	"github.com/EducationEKT/EKT/schema"
 )
 
 func SaveReceiptByTxHash(chainId int64, txHash string, detail userevent.ReceiptDetail) {
-	key := schema.GetReceiptByTxHash(chainId, txHash)
+	key := schema.GetReceiptByTxHashKey(chainId, txHash)
 	db.GetDBInst().Set(key, detail.Bytes())
 }
 
 func GetReceiptByTxHash(chainId int64, txHash string) *userevent.ReceiptDetail {
-	key := schema.GetReceiptByTxHash(chainId, txHash)
+	key := schema.GetReceiptByTxHashKey(chainId, txHash)
 	data, err := db.GetDBInst().Get(key)
 	if err != nil {
 		return nil
