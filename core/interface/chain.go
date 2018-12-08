83fe56a94c6a63551c5505e24c8e20a357dc334c
zhouxun <xlgfzh@gmail.com>
2018-12-08 19:41:59 +0800

    interface add method

--- core/interface/chain.go
+++ core/interface/chain.go
@@ -4,4 +4,10 @@ import "github.com/EducationEKT/EKT/core/types"
 
 type ChainReader interface {
 	GetAccount(address []byte) (*types.Account, error)
+
+	Author() []byte
+
+	GetTimestamp() int64
+
+	GetParent() []byte
 }
