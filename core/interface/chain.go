2d3da7484749edd6db8f79a0e99ec813840da9cd
zhouxun <xlgfzh@gmail.com>
2018-12-11 21:23:12 +0800

    修改VMChain接口

--- core/interface/chain.go
+++ core/interface/chain.go
@@ -2,6 +2,11 @@ package _interface
 
 import "github.com/EducationEKT/EKT/core/types"
 
+type VMChain interface {
+	ChainReader
+	ChainWriter
+}
+
 type ChainReader interface {
 	GetAccount(address []byte) (*types.Account, error)
 
@@ -10,4 +15,8 @@ type ChainReader interface {
 	GetTimestamp() int64
 
 	GetParent() []byte
+}
+
+type ChainWriter interface {
+	ModifyContract(address, data []byte) error
 }
