56e6540c58363e2c9bf573e2b89d5a2e6c3de4ba
zhouxun <xlgfzh@gmail.com>
2019-01-15 20:25:26 +0800

    优化细节

--- downloader/downloader.go
+++ downloader/downloader.go
@@ -3,12 +3,18 @@ package downloader
 import (
 	"bytes"
 	"encoding/hex"
+
+	"github.com/EducationEKT/EKT/conf"
 	"github.com/EducationEKT/EKT/crypto"
 	"github.com/EducationEKT/EKT/param"
 )
 
 func Synchronise(key []byte) []byte {
 	for _, node := range param.MainChainDelegateNode {
+		if node.Equal(conf.EKTConfig.Node) {
+			continue
+		}
+
 		value, err := node.GetDBValue(hex.EncodeToString(key))
 		if err == nil && bytes.Equal(crypto.Sha3_256(value), key) {
 			return value
