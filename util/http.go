3f8e9a9fed7d7c61316854c40bb12da468378090
zhouxun <xlgfzh@gmail.com>
2019-03-05 17:40:05 +0800

    solve too many TIME_WAIT

--- util/http.go
+++ util/http.go
@@ -4,22 +4,10 @@ import (
 	"bytes"
 	"io/ioutil"
 	"net/http"
-	"runtime"
-	"time"
 )
 
-func init() {
-	http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = runtime.NumGoroutine()
-}
-
 func HttpGet(url string) ([]byte, error) {
-	client := &http.Client{}
-	client.Timeout = 2 * time.Second
-	request, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-	resp, err := client.Do(request)
+	resp, err := http.Get(url)
 	if resp != nil {
 		defer resp.Body.Close()
 	}
@@ -30,14 +18,7 @@ func HttpGet(url string) ([]byte, error) {
 }
 
 func HttpPost(url string, body []byte) ([]byte, error) {
-	client := &http.Client{}
-	client.Timeout = 2 * time.Second
-	request, err := http.NewRequest("POST", url, bytes.NewReader(body))
-	if err != nil {
-		return nil, err
-	}
-	request.Header["Content-Type"] = []string{"application/json"}
-	resp, err := client.Do(request)
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(body))
 	if resp != nil {
 		defer resp.Body.Close()
 	}
